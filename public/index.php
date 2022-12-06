91c39f70292abef38e7983cbc2dfcf843de63fe7
apaple <76417012+apaple@users.noreply.github.com>
2022-12-06 14:33:35 +0700

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,55 +1,268 @@
-<?php
 
-use Illuminate\Contracts\Http\Kernel;
-use Illuminate\Http\Request;
 
-define('LARAVEL_START', microtime(true));
+<!DOCTYPE html>
+<html>
+<head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<!--[if lt IE 9]><script src="js/html5.js"></script><![endif]-->
+<title> - Best IP Stresser / DDOS Booter 2021 - ripstresser.top</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<meta name="keywords" content="stresser, booter, stress test, ip booter, ip stresser, best stresser, web stresser, stress them, best booter, DDoS, Booter, Stresser, Free Stresser, Free Booter, Free DDoS, DDoS Tool, Best IP Booter, Best IP Stresser, Best DDoS Tool, Mirai Booter, ddos free, ddos ovh, down ovh, bypass ovh, best stresser 2021, best booter 2021, top best free stresser" />
+<meta name="description" content="Free new ddos booters or ip stressers are useless. ripstresser.top is the best web stresser or ip booter of 2021. Stress them all with our ddos service!" />
+<script src="/cdn-cgi/apps/head/d8HLQchpyyhAYVRu8V1MYrIVnT0.js"></script><link href="new/css/style.css?45" rel="stylesheet" type="text/css" />
+<script src='https://www.google.com/recaptcha/api.js' async type="e68179e7b5bd8616b74cca8f-text/javascript"></script>
 
-/*
-|--------------------------------------------------------------------------
-| Check If Application Is Under Maintenance
-|--------------------------------------------------------------------------
-|
-| If the application is maintenance / demo mode via the "down" command we
-| will require this file so that any prerendered template can be shown
-| instead of starting the framework, which could cause an exception.
-|
-*/
+<script async src="https://www.googletagmanager.com/gtag/js?id=AW-700921590" type="e68179e7b5bd8616b74cca8f-text/javascript"></script>
+<script type="e68179e7b5bd8616b74cca8f-text/javascript">
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
-if (file_exists(__DIR__.'/../storage/framework/maintenance.php')) {
-    require __DIR__.'/../storage/framework/maintenance.php';
+  gtag('config', 'AW-700921590');
+</script>
+
+<script type="e68179e7b5bd8616b74cca8f-text/javascript">
+  gtag('event', 'conversion', {'send_to': 'AW-700921590/9rxfCP-Eu94BEPbtnM4C'});
+</script>
+<meta name="mailru-domain" content="rYUTSSw2zG4Jx4oT" />
+<style>
+@media (max-width: 899px)
+.logobig img{
+       display: none;
 }
+</style>
+</head>
+<body>
+<div class="first">
+<div class="video">
+<iframe src="https://www.youtube.com/embed/ptHpUuq-Zt0?mode=opaque&wmode=transparent&autoplay=1&loop=1&controls=0&modestbranding=1&rel=0&autohide=1&showinfo=0&color=white&iv_load_policy=3&theme=light&wmode=transparent&mute=1&playlist=ptHpUuq-Zt0" frameborder="0" allow="autoplay" style="    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;"></iframe>
 
-/*
-|--------------------------------------------------------------------------
-| Register The Auto Loader
-|--------------------------------------------------------------------------
-|
-| Composer provides a convenient, automatically generated class loader for
-| this application. We just need to utilize it! We'll simply require it
-| into the script here so we don't need to manually load our classes.
-|
-*/
+</div>
+<div class="wrapper">
+<div class="head flex">
+<div class="logo">
+<div class="logobig"><a href="/"><img alt="" class="b-lazy svg" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" data-src="new/images/logo.svg?2" /></a></div>
+<div class="logosmall"><a href="/"><img src="new/images/logo.png" alt="" /></a></div>
+</div>
+<div class="topmenu box">
+<ul>
+<li><a href="">HOME</a></li>
+<li><a href="">FUTURES</a></li>
+<li><a href="">ADVANTAGE</a></li>
+<li><a href="">PRICE</a></li>
+<li><a class="inlogin" href="#reg">REGISTER</a></li>
+<li><a class="inlogin" href="#login" id="regButton">LOGIN</a></li>
+</ul>
+</div>
+</div>
+<div class="slogan">
+<div class="slogan_title"><h1>BEST IP STRESSER IN THE WORLD</h1></div>
+<div class="slogan_title2">GREAT STRONG IP BOOTER STRESSER</div>
+<div class="slogan_links">
+<ul>
+<li><a href="#reg">REGISTER</a></li>
+<li><a href="#login">LOGIN</a></li>
+</ul>
+</div>
+</div>
+</div>
+</div>
+<div class="second box">
+<div class="wrapper">
+<div class="second_flex flex box">
+<div class="second_slogan">
+<div class="slogan_title">FEATURES ripstresser.top</div>
+<div class="slogan_title2">Our dedicated servers are connected to an unshared 100Gb/s port, to guarantee you power.</div>
+<div class="slogan_title2">All your attacks pass on our servers that use a technology called « IP spoofing ».</div>
+<div class="slogan_title2">We offer the best performance in terms of innovation and infrastructure.</div>
+</div>
+<div class="block_links">
+<div class="theblock theblock1">
+<a href="#">
+<div class="block_ico"><img src="new/images/prime_ico.png" alt="" /></div>
+<div class="block_title">PRICE</div>
+<div class="block_more">More details <i class="fa fa-angle-double-right" aria-hidden="true"></i></div>
+</a>
+</div>
+<div class="theblock theblock2">
+<a href="#">
+<div class="block_ico"><img src="new/images/news_ico.png" alt="" /></div>
+<div class="block_title">NEWS</div>
+<div class="block_more">More details <i class="fa fa-angle-double-right" aria-hidden="true"></i></div>
+</a>
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class="the_text">
+<div class="wrapper">
+<div class="text_title">ripstresser.top INDIVIDUAL FEATURES</div>
+<p>Highly powerful ddos attacks capable of taking large websites and servers offline.</p>
+<p>Stress tests are launched from multiple locations (botnet) and can't be traced.</p>
+<p> Your privacy is safe with us, no logs are kept and all data is encrypted.</p>
+<p> Our ddos attack methods are capable of bypassing the latest protections.</p>
+<p>You can use our web stresser from mobile phones, computers or tablets.</p>
+<p> Unlike our competitors, our booter is custom made, a proof of our skills.</p>
+</div>
+</div>
+<div class="third box">
+<div class="wrapper">
+<div class="second_flex flex box">
+<div class="second_slogan">
+</div>
+<div class="block_links">
+<div class="theblock theblock1">
+<a href="#">
+<div class="block_ico"><img src="new/images/team_ico.png" alt="" /></div>
+<div class="block_title">SUPPORT</div>
+<div class="block_more">More details <i class="fa fa-angle-double-right" aria-hidden="true"></i></div>
+</a>
+</div>
+<div class="theblock theblock2">
+<a href="#">
+<div class="block_ico"><img src="new/images/quest_ico.png" alt="" /></div>
+<div class="block_title">FAQ</div>
+<div class="block_more">More details <i class="fa fa-angle-double-right" aria-hidden="true"></i></div>
+</a>
+</div>
+</div>
+</div>
+</div>
+</div>
+<footer class="footer">
+<div class="footer_line1">
+<div class="wrapper flex">
+<div class="footer_logo"><a href="/"><img src="new/images/logo.png" alt="" /></a></div>
+<div class="footer_menu">
+<ul>
+<li><a href="">HOME</a></li>
+<li><a href="">FUTURES</a></li>
+<li><a href="">ADVANTAGE</a></li>
+<li><a href="">PRICE</a></li>
+<li><a href="#reg">REGISTER</a></li>
+<li><a href="#login">LOGIN</a></li>
+</ul>
+</div>
+</div>
+</div>
+<div class="footer_line2">
+<div class="wrapper flex">
+<div class="politics"><a href="#">ripstresser.top</a></div>
+<div class="copyright">&Copyright 2019-2021. All rights reserved</div>
+<div class="soc">
+<a href="#" class="box inline-box"><i class="fa fa-vk" aria-hidden="true"></i></a>
+<a href="#" class="box inline-box"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+<a href="#" class="box inline-box"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+<a href="#" class="box inline-box"><i class="fa fa-youtube-play" aria-hidden="true"></i></a>
+</div>
+</div>
+</div>
+</footer>
+<div id="reg" class="white-popup-block mfp-hide">
+<form method="post" role="form" id="register">
+<div class="form_name">Registration</div>
+<div class="error_msg2"></div>
+<div><input type="text" class="box" name="username" id="username" placeholder="Username" autocomplete="off" /></div>
+<div><input type="email" class="box" name="email" id="email" placeholder="E-Mail Address" autocomplete="off" /></div>
+<div><input type="password" class="box" name="passwd" id="passwd2" placeholder="Password" autocomplete="off" /></div>
+<div><input type="password" class="box" name="rpasswd" id="rpasswd" placeholder="Confirm Password" autocomplete="off" /></div>
 
-require __DIR__.'/../vendor/autoload.php';
+<div class="g-recaptcha" data-sitekey="6LeOgRwdAAAAAHyRjwPChFE5QqOPLL7rlWUkkFBH"></div>
 
-/*
-|--------------------------------------------------------------------------
-| Run The Application
-|--------------------------------------------------------------------------
-|
-| Once we have the application, we can handle the incoming request using
-| the application's HTTP kernel. Then, we will send the response back
-| to this client's browser, allowing them to enjoy our application.
-|
-*/
+<div>
+<br />
+<label style="font-size: 20px;color: green;">
+<input type="checkbox" name="terms" value="agree" style=" height: 20px;width: 20px;    outline: 2px solid red;">
+By registering, I agree to your <a href="tos.php" target="_blank"><font color="red">Terms of Service</font></a>.
+</label>
+<br />
+</div>
+<input type="hidden" name="ref" id="ref" value="" />
+<div><input type="submit" name="registerBtn" class="box" value="Register" /></div>
+</form>
+</div>
+<div id="login" class="white-popup-block mfp-hide">
+<div class="form_name">Login to Ripstresser panel!</div>
+<div class="error_msg"></div>
+<form method="post" role="form" id="login">
+<div><input type="text" class="box" name="username" id="username" placeholder="Username" /></div>
+<div><input type="password" class="box" name="passwd" id="passwd" placeholder="Password" /></div>
+<div><input type="submit" class="box" value="Log In" name="loginBtn" /></div>
+</form>
+</div>
 
-$app = require_once __DIR__.'/../bootstrap/app.php';
+<script async src="assets/js/bootstrap.min.js" type="e68179e7b5bd8616b74cca8f-text/javascript"></script>
+<script async src="assets/js/resizeable.js" type="e68179e7b5bd8616b74cca8f-text/javascript"></script>
+<script async src="assets/js/jquery-validate/jquery.validate.min.js" type="e68179e7b5bd8616b74cca8f-text/javascript"></script>
+<script async src="assets/js/toastr/toastr.min.js" type="e68179e7b5bd8616b74cca8f-text/javascript"></script>
+<script type="e68179e7b5bd8616b74cca8f-text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+<script async type="e68179e7b5bd8616b74cca8f-text/javascript" src="new/js/custom.js?49"></script>
+<link href="new/css/magnific-popup.css" rel="stylesheet" type="text/css" />
+<link href="https://fonts.googleapis.com/css?family=Bebas+Neue|Montserrat:400,400i,500,500i,700,700i&display=swap&subset=cyrillic-ext,latin-ext" rel="stylesheet">
+<link href="new/css/responsive.css?25" rel="stylesheet" type="text/css" />
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Arimo:400,700,400italic">
+<link rel="stylesheet" href="assets/css/fonts/linecons/css/linecons.css">
+<link rel="stylesheet" href="assets/css/custom.css">
+<script src="blazy.min.js" type="e68179e7b5bd8616b74cca8f-text/javascript"></script>
+<script type="e68179e7b5bd8616b74cca8f-text/javascript">
+    ;(function() {
+        // Initialize
+        var bLazy = new Blazy();
+     })();
+</script>
 
-$kernel = $app->make(Kernel::class);
+<script type="e68179e7b5bd8616b74cca8f-text/javascript">
+   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-$response = tap($kernel->handle(
-    $request = Request::capture()
-))->send();
+   ym(86316009, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+   });
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/86316009" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
 
-$kernel->terminate($request, $response);
+<script type="e68179e7b5bd8616b74cca8f-text/javascript">
+    const refreshCaptcha = (target) => {
+      const captchaImage = target.closest('.captcha__image-reload').querySelector('.captcha__image');
+      captchaImage.src = 'captcha/captcha.php?r=' + new Date().getUTCMilliseconds();
+    }
+	
+	const captchaBtn = document.querySelector('.captcha__refresh');
+// запуск функции refreshCaptcha при нажатии на кнопку
+captchaBtn.addEventListener('click', (e) => refreshCaptcha(e.target));
+</script>
+<style>
+.captcha__refresh {
+    margin-left: 0.5rem;
+    display: inline-flex;
+    align-items: center;
+    font-weight: 400;
+    line-height: 1.5;
+    color: #000;
+    text-decoration: none;
+    cursor: pointer;
+    user-select: none;
+    background-color: #f5f5f5;
+    border: 1px solid #f5f5f5;
+    padding: 0.375rem 0.75rem;
+    font-size: 1rem;
+    border-radius: 0.25rem;
+    transition: background-color .15s ease-in-out, border-color .15s ease-in-out;
+}
+.captcha__refresh::before {
+      content: "";
+      width: 16px;
+      height: 16px;
+      background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-arrow-repeat' viewBox='0 0 16 16'%3E%3Cpath d='M11.534 7h3.932a.25.25 0 0 1 .192.41l-1.966 2.36a.25.25 0 0 1-.384 0l-1.966-2.36a.25.25 0 0 1 .192-.41zm-11 2h3.932a.25.25 0 0 0 .192-.41L2.692 6.23a.25.25 0 0 0-.384 0L.342 8.59A.25.25 0 0 0 .534 9z'/%3E%3Cpath fill-rule='evenodd' d='M8 3c-1.552 0-2.94.707-3.857 1.818a.5.5 0 1 1-.771-.636A6.002 6.002 0 0 1 13.917 7H12.9A5.002 5.002 0 0 0 8 3zM3.1 9a5.002 5.002 0 0 0 8.757 2.182.5.5 0 1 1 .771.636A6.002 6.002 0 0 1 2.083 9H3.1z'/%3E%3C/svg%3E");
+    }
+</style>
+<script src="/cdn-cgi/scripts/7d0fa10a/cloudflare-static/rocket-loader.min.js" data-cf-settings="e68179e7b5bd8616b74cca8f-|49" defer=""></script></body>
+</html>
